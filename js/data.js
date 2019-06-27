1ded8fd86c4115644f47302c88adf12d410fc6d2
Artur Pokrywka <pokrywkaart@gmail.com>
2019-06-27 16:30:09 +0200

    First react app

---
+++ js/data.js
@@ -0,0 +1,32 @@
+let movies = [
+    {
+        id: 1,
+        title: 'Harry Potter',
+        desc: 'Film o czarodzieju',
+        poster: './images/harry-potter.jpg'
+    },
+    {
+        id: 2,
+        title: 'Król Lew',
+        desc: 'Film opowiadający historię króla sawanny',
+        poster: './images/lion-king.jpg'
+    },
+    {
+        id: 3,
+        title: 'Podziemny krąg',
+        desc: 'Dwóch mężczyzn znudzonych rutyną zakłada klub, w którym co tydzień odbywają się walki na gołe pięści',
+        poster: './images/fight-club.jpg'
+    },
+    {
+        id: 4,
+        title: 'Coś',
+        desc: 'W odciętej od świata bazie badawczej na Antarktydzie coś morduje naukowców',
+        poster: './images/thing.jpg'
+    },
+    {
+        id: 5,
+        title: 'Bękarty wojny',
+        desc: 'W okupowanej przez nazistów Francji oddział złożony z Amerykanów żydowskiego pochodzenia planuje zamach na Hitlera',
+        poster: './images/ing-bastards.jpg'
+    },
+];
---
+++ js/script.js
@@ -0,0 +1,15 @@
+let moviesElements = movies.map(function(movie) {
+    return  React.createElement('li', {key: movie.id},
+    React.createElement('h2', {}, movie.title),
+    React.createElement('p', {}, movie.desc),
+    React.createElement('img', {src: movie.poster})
+    );
+});
+
+let moviesList = 
+    React.createElement('div', {},
+        React.createElement('h1', {}, 'Lista filmów'),
+        React.createElement('ul', {}, moviesElements)
+    );    
+
+ReactDOM.render(moviesList, document.getElementById('app'));
