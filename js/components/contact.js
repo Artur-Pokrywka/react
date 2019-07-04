5ece4385ed34da52ebfb08ffba22285aae409ff2
Artur Pokrywka <pokrywkaart@gmail.com>
2019-07-04 15:59:55 +0200

    Add Contacts class

---
+++ js/components/contact.js
@@ -0,0 +1,22 @@
+let Contact = React.createClass({
+    propTypes: {
+        item: React.PropTypes.object.isRequired,
+    },
+
+    render: function() {
+        return (
+            React.createElement('li', {className: 'contactItem'},
+                React.createElement('img', {
+                    className: "contactImage",
+                    src: 'https://picsum.photos/200'
+                }),
+                React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
+                React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+                React.createElement('a', {className: 'conatactEmail', href: 'mailto:' + this.props.item.email},
+                    this.props.item.email
+                )    
+            )
+        )
+    },
+});
+
