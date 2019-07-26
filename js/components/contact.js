9c24d0d70c6c1a86408f07f8543d1f80ed9cfcd0
Artur Pokrywka <pokrywkaart@gmail.com>
2019-07-26 17:22:35 +0200

    Implementing JSX

--- js/components/contact.js
+++ js/components/contact.js
@@ -5,17 +5,29 @@ let Contact = React.createClass({
 
     render: function() {
         return (
-            React.createElement('li', {className: 'contactItem'},
-                React.createElement('img', {
-                    className: "contactImage",
-                    src: 'https://picsum.photos/200'   // link z Kodilli nie działał 
-                }),
-                React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-                React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-                React.createElement('a', {className: 'conatactEmail', href: 'mailto:' + this.props.item.email},
-                    this.props.item.email
-                )    
-            )
+            // React.createElement('li', {className: 'contactItem'},
+            //     React.createElement('img', {
+            //         className: "contactImage",
+            //         src: 'https://picsum.photos/200'  
+            //     }),
+            //     React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
+            //     React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+            //     React.createElement('a', {className: 'conatactEmail', href: 'mailto:' + this.props.item.email},
+            //         this.props.item.email
+            //     )    
+            // )
+            <li className={'contactItem'}>
+                <img className={'contactImage'} src={'https://picsum.photos/200'}/>
+                <p className={'contactLabel'}>
+                    Imię: {this.props.item.firstName}
+                </p>
+                <p className={'contactLabel'}>
+                    Nazwisko: {this.props.item.lastName}
+                </p>
+                <a className={'contactEmail'} href={'mailto:' + this.props.item.email}>
+                    {this.props.item.email}
+                </a>
+            </li>
         )
     },
 });
