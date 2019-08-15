1991ec6bbad25ec1bfb47102fa2b70f2e3d007c1
Artur Pokrywka <pokrywkaart@gmail.com>
2019-08-15 12:54:29 +0200

    Did nothing

--- js/script.js
+++ js/script.js
@@ -1,79 +1,47 @@
 class Stopwatch extends React.Component {
-    constructor(display) {
-        super(display);
+    constructor(props) {
+        super(props);
         this.state = {
             running: false,
-            txt: 'hello world',
-            display: display,   
-            // print: print(),
-            // reset: reset()
+            time:  `00 : 00 : 00 `,
+            start: 0,
         }
+        this.start = this.start.bind(this)
+        this.stop = this.stop.bind(this)
+        this.clear = this.stop.bind(this)
     }
-
-    reset() {
-        this.state.times = {
-            minutes: 0,
-            seconds: 0,
-            miliseconds: 0 
-        };
-        console.log('times', this.state.times)
-    }
-
-    print() {
-        this.state.display.innerText = this.format(this.state.times);
-      
-    }
-
-    format(times) {
-        let pad0 = (value) => {
-            let result = value.toString();
-            if (result.length < 2) {
-                result = '0' + result;
-            }
-            return result;
-        };
-        return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
-    }  
 
     start() {
-        console.log('this is working');
-        if (!this.running) {
-            this.state = {
-                running: true
-            }
-          
-            this.state.watch = setInterval(() => this.step(), 10);
-        }
-    }
-    
-    step() {
-        if (!this.running) return;
-        this.calculate();
-        this.print();
-    }
-    
-    calculate() {
-        this.times.miliseconds += 1;
-        if (this.times.miliseconds >= 100) {
-            this.times.seconds += 1;
-            this.times.miliseconds = 0;
-        }
-        if (this.times.seconds >= 60) {
-            this.times.minutes += 1;
-            this.times.seconds = 0;
-        }
+        
+        this.setState = ({
+            running: true,
+            time: this.state.time,
+            start: Date.now() - this.state.time
+        })
+        this.timer = setInterval( () => this.setState({
+            time: Date.now() - this.state.start
+        }), 1);
+        
+        console.log('stan', this.state)
     }
     
     stop() {
-        this.running = false;
-        clearInterval(this.watch);
+        this.setState = ({
+            running: false
+        })
+        clearInterval(this.timer)
+
+        console.log('stan', this.state)
     }
 
-    
     clear() {
-        this.running = false;
-        this.reset();
-        this.print(this.times);
+        if (this.state.time != 0) {
+            this.setState = ({
+                running: false,
+                time:  `00 : 00 : 00 `
+            })
+        }
+        console.log('stan', this.state)
     }
     
 
@@ -85,8 +53,7 @@ class Stopwatch extends React.Component {
                     <button onClick={this.stop}> Stop </button>
                     <button onClick={this.clear}> Clear </button>
                 </nav>
-                <ul>{this.state.times}</ul>
-                <p>{this.state.txt}</p>
+                <p>{this.state.time}</p>
             </div>
             
         );
