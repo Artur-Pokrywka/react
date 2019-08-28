5217eab8d1ffa741b18ae4438643811f010ed18a
Artur Pokrywka <pokrywkaart@gmail.com>
2019-08-28 10:29:35 +0200

    Did nothing

--- js/components/App.js
+++ js/components/App.js
@@ -18,9 +18,14 @@ App = class App extends React.Component {
       const url = `https://api.github.com/search/users?q=${searchText}`;
       fetch(url)
         .then(response => response.json())
-        .then(responseJson => this.setState({users: responseJson.items}))
-        .catch(error => this.setState({error: 'coś poszło nie tak'}));
+        
+        .then(responseJson => this.setState({users: responseJson.items}))    
+        .catch(error => this.setState({error: 'coś poszło nie tak'}))
+        // if (this.users.length === 0 ) {
+        //   error => this.setState({error: 'brak wyników'})
+        // }
         console.log('lol', this.state.users)
+        // console.log('lol', this.responseJson.items)
     }
   
     render() {
